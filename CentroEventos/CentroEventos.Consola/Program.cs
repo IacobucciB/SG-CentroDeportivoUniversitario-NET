b189f2dab86ae31a1999a42c7ea59a84738671b1
IacobucciB <link00222@gmail.com>
2025-05-19 01:05:19 -0300

    feat: Consola EventoDeportivo working

--- CentroEventos/CentroEventos.Consola/Program.cs
+++ CentroEventos/CentroEventos.Consola/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using CentroEventos.Aplicacion;
+using CentroEventos.Repositorios;
+
+// Configuro las dependencias
+IRepositorioEventoDeportivo repo = new RepositorioEventoDeportivoTXT();
+// Creo los casos de uso
+var altaEvento = new AltaEventoDeportivoUseCase(repo);
+var listarEventos = new ListarEventosDeportivosUseCase(repo);
+// Ejecuto los casos de uso
+altaEvento.Ejecutar(new EventoDeportivo() {
+    Id = 1,
+    Nombre = "Fútbol 5",
+    Descripcion = "Torneo de fútbol 5 universitario",
+    FechaHoraInicio = DateTime.Now.AddDays(1),
+    DuracionHoras = 2,
+    CupoMaximo = 10,
+    ResponsableId = 1
+});
+altaEvento.Ejecutar(new EventoDeportivo() {
+    Id = 2,
+    Nombre = "Básquet",
+    Descripcion = "Encuentro amistoso de básquet",
+    FechaHoraInicio = DateTime.Now.AddDays(2),
+    DuracionHoras = 1.5,
+    CupoMaximo = 12,
+    ResponsableId = 2
+});
+var lista = listarEventos.Ejecutar();
+foreach (EventoDeportivo e in lista)
+{
+    Console.WriteLine(e);
+}
---
+++ CentroEventos/CentroEventos.Repositorios/RepositorioPersonaTXT.cs
@@ -0,0 +1,98 @@
+namespace CentroEventos.Repositorios;
+
+using CentroEventos.Aplicacion;
+
+public class RepositorioPersonaTXT : IRepositorioPersona
+{
+    readonly string _nombreArch = "persona.txt";
+    readonly string _ultimoIdArch = "ultimoIdPersona.txt";
+
+    public int ObtenerNuevoId()
+    {
+        int ultimoId = 0;
+        if (File.Exists(_ultimoIdArch))
+        {
+            ultimoId = int.Parse(File.ReadAllText(_ultimoIdArch));
+        }
+        ultimoId++;
+        File.WriteAllText(_ultimoIdArch, ultimoId.ToString());
+        return ultimoId;
+    }
+
+    public void AltaPersona(Persona persona)
+    {
+        persona.Id = ObtenerNuevoId();
+        using var sw = new StreamWriter(_nombreArch, true);
+        sw.WriteLine(persona.Id);
+        sw.WriteLine(persona.DNI);
+        sw.WriteLine(persona.Nombre);
+        sw.WriteLine(persona.Apellido);
+        sw.WriteLine(persona.Email);
+        sw.WriteLine(persona.Telefono);
+    }
+
+    public void BajaPersona(int id)
+    {
+        var personas = ListarPersonas();
+        using var sw = new StreamWriter(_nombreArch);
+        foreach (var persona in personas)
+        {
+            if (persona.Id != id)
+            {
+                sw.WriteLine(persona.Id);
+                sw.WriteLine(persona.DNI);
+                sw.WriteLine(persona.Nombre);
+                sw.WriteLine(persona.Apellido);
+                sw.WriteLine(persona.Email);
+                sw.WriteLine(persona.Telefono);
+            }
+        }
+    }
+
+    public void ModificarPersona(Persona personaModificada)
+    {
+        var personas = ListarPersonas();
+        using var sw = new StreamWriter(_nombreArch);
+        foreach (var persona in personas)
+        {
+            if (persona.Id == personaModificada.Id)
+            {
+                sw.WriteLine(personaModificada.Id);
+                sw.WriteLine(personaModificada.DNI);
+                sw.WriteLine(personaModificada.Nombre);
+                sw.WriteLine(personaModificada.Apellido);
+                sw.WriteLine(personaModificada.Email);
+                sw.WriteLine(personaModificada.Telefono);
+            }
+            else
+            {
+                sw.WriteLine(persona.Id);
+                sw.WriteLine(persona.DNI);
+                sw.WriteLine(persona.Nombre);
+                sw.WriteLine(persona.Apellido);
+                sw.WriteLine(persona.Email);
+                sw.WriteLine(persona.Telefono);
+            }
+        }
+    }
+
+    public List<Persona> ListarPersonas()
+    {
+        var resultado = new List<Persona>();
+        if (!File.Exists(_nombreArch))
+            return resultado;
+        using var sr = new StreamReader(_nombreArch);
+        while (!sr.EndOfStream)
+        {
+            var persona = new Persona();
+            persona.Id = int.Parse(sr.ReadLine()!);
+            persona.DNI = sr.ReadLine()!;
+            persona.Nombre = sr.ReadLine()!;
+            persona.Apellido = sr.ReadLine()!;
+            persona.Email = sr.ReadLine()!;
+            persona.Telefono = sr.ReadLine()!;
+            resultado.Add(persona);
+        }
+        return resultado;
+    }
+}
