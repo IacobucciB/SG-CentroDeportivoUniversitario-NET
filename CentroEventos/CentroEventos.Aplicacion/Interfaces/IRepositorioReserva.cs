020437570fe9cf6404ca89bc75c8f780937ef380
Princeps13 <princeps13cs1.6@gmail.com>
2025-05-19 01:00:40 -0300

    agregue la clase IrepositorioReserva

--- CentroEventos/CentroEventos.Aplicacion/Interfaces/IRepositorioReserva.cs
+++ CentroEventos/CentroEventos.Aplicacion/Interfaces/IRepositorioReserva.cs
@@ -2,5 +2,11 @@ namespace CentroEventos.Aplicacion;
 
 public interface IRepositorioReserva
 {
-    // TO DO
+   void Agregar(Reserva reserva);
+    void Modificar(Reserva reserva);
+    void Eliminar(int id);
+    //Reserva? ObtenerPorId(int id);
+    List<Reserva> ListarTodas();
+    List<Reserva> ObtenerPorEvento(int eventoId);
+    bool ExisteReservaDePersonaEnEvento(int personaId, int eventoId);
 }
