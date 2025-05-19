b8bb766b359d3d680fa2d03add8993878cc00ed3
IacobucciB <link00222@gmail.com>
2025-05-19 02:11:59 -0300

    fix: fixed typo

--- CentroEventos/CentroEventos.Repositorios/RepositorioPersonaTXT.cs
+++ CentroEventos/CentroEventos.Repositorios/RepositorioPersonaTXT.cs
@@ -1,14 +1,10 @@
 namespace CentroEventos.Repositorios;
 
-<<<<<<< HEAD
-=======
-using System.Security.Cryptography.X509Certificates;
->>>>>>> origin/personaNeme
+
 using CentroEventos.Aplicacion;
 
 public class RepositorioPersonaTXT : IRepositorioPersona
 {
-<<<<<<< HEAD
     readonly string _nombreArch = "persona.txt";
     readonly string _ultimoIdArch = "ultimoIdPersona.txt";
 
@@ -24,14 +20,9 @@ public class RepositorioPersonaTXT : IRepositorioPersona
         return ultimoId;
     }
 
-    public void AltaPersona(Persona persona)
-    {
-        persona.Id = ObtenerNuevoId();
-=======
-    readonly string _nombreArch = "personas.txt";
     public void AgregarPersona(Persona persona)
     {
->>>>>>> origin/personaNeme
+        persona.Id = ObtenerNuevoId();
         using var sw = new StreamWriter(_nombreArch, true);
         sw.WriteLine(persona.Id);
         sw.WriteLine(persona.DNI);
@@ -40,9 +31,8 @@ public class RepositorioPersonaTXT : IRepositorioPersona
         sw.WriteLine(persona.Email);
         sw.WriteLine(persona.Telefono);
     }
-<<<<<<< HEAD
 
-    public void BajaPersona(int id)
+    public void EliminarPersona(int id)
     {
         var personas = ListarPersonas();
         using var sw = new StreamWriter(_nombreArch);
@@ -92,50 +82,18 @@ public class RepositorioPersonaTXT : IRepositorioPersona
         var resultado = new List<Persona>();
         if (!File.Exists(_nombreArch))
             return resultado;
-=======
-    public List<Persona> ListarPersonas()
-    {
-        var resultado = new List<Persona>();
->>>>>>> origin/personaNeme
         using var sr = new StreamReader(_nombreArch);
         while (!sr.EndOfStream)
         {
             var persona = new Persona();
-<<<<<<< HEAD
             persona.Id = int.Parse(sr.ReadLine()!);
             persona.DNI = sr.ReadLine()!;
             persona.Nombre = sr.ReadLine()!;
             persona.Apellido = sr.ReadLine()!;
             persona.Email = sr.ReadLine()!;
             persona.Telefono = sr.ReadLine()!;
-=======
-            persona.Id = int.Parse(sr.ReadLine() ?? "");
-            persona.DNI = sr.ReadLine() ?? "";
-            persona.Nombre = sr.ReadLine() ?? "";
-            persona.Apellido = sr.ReadLine() ?? "";
-            persona.Email = sr.ReadLine() ?? "";
-            persona.Telefono = sr.ReadLine() ?? "";
->>>>>>> origin/personaNeme
             resultado.Add(persona);
         }
         return resultado;
     }
-<<<<<<< HEAD
 }
-=======
-    public void ModificarPersona(Persona persona)
-    {
-        var personas = ListarPersonas();
-        var personaModificar = personas.FirstOrDefault(p => p.Id == persona.Id);
-        if (personaModificar != null)
-        {
-            personaModificar.DNI = persona.DNI;
-            personaModificar.Nombre = persona.Nombre;
-            personaModificar.Apellido = persona.Apellido;
-            personaModificar.Email = persona.Email;
-            personaModificar.Telefono = persona.Telefono;
-        }
-        //GuardarCambios(personas);
-    }
-}
->>>>>>> origin/personaNeme
