98933c43d274cf915e5f23bfe571f9d19eedf6d5
IacobucciB <link00222@gmail.com>
2025-05-20 00:37:08 -0300

    feat: useCase mas completos

--- CentroEventos/CentroEventos.Aplicacion/UseCase/BajaReservaUseCase.cs
+++ CentroEventos/CentroEventos.Aplicacion/UseCase/BajaReservaUseCase.cs
@@ -4,6 +4,12 @@ public class BajaReservaUseCase(IRepositorioReserva repositorioReserva)
 {
     public void Ejecutar(int id)
     {
+        // 1. Validar existencia de la reserva
+        var reserva = repositorioReserva.ObtenerReservaPorId(id);
+        if (reserva == null)
+            throw new EntidadNotFoundException("La reserva no existe.");
+
+        // 2. Eliminar reserva
         repositorioReserva.BajaReserva(id);
     }
 }
--- CentroEventos/CentroEventos.Aplicacion/UseCase/ModificarReservaUseCase.cs
+++ CentroEventos/CentroEventos.Aplicacion/UseCase/ModificarReservaUseCase.cs
@@ -4,6 +4,12 @@ public class ModificarReservaUseCase(IRepositorioReserva repositorioReserva)
 {
     public void Ejecutar(Reserva reserva)
     {
+        // 1. Validar existencia de la reserva
+        var reservaExistente = repositorioReserva.ObtenerReservaPorId(reserva.Id);
+        if (reservaExistente == null)
+            throw new EntidadNotFoundException("La reserva no existe.");
+
+        // 2. Modificar reserva
         repositorioReserva.ModificarReserva(reserva);
     }
 }
