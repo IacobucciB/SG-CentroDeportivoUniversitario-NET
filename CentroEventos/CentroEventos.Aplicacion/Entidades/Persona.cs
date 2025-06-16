d76586a947bc9b42c7843df7581e6d51719d97e7
IacobucciB <link00222@gmail.com>
2025-06-16 14:24:21 -0300

    updated Persona Usuario, contrasena y ListaPermisos

--- CentroEventos/CentroEventos.Aplicacion/Entidades/Persona.cs
+++ CentroEventos/CentroEventos.Aplicacion/Entidades/Persona.cs
@@ -3,14 +3,16 @@ namespace CentroEventos.Aplicacion;
 public class Persona
 {
     public int Id { get; set; }
-    public string DNI { get; set; } = "";
-    public string Nombre { get; set; } = "";
-    public string Apellido { get; set; } = "";
-    public string Email { get; set; } = "";
-    public string Telefono { get; set; } = "";
-
+    public string DNI { get; set; } = string.Empty;
+    public string Nombre { get; set; } = string.Empty;
+    public string Apellido { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Telefono { get; set; } = string.Empty;
+    public string Contrasena { get; set; } = string.Empty;
+    public List<Permiso> ListaPermisos { get; set; } = new List<Permiso>();
     public override string ToString()
     {
-        return $"Persona: {Id}, {DNI}, {Nombre}, {Apellido}, {Email}, {Telefono}";
+        var permisos = string.Join(", ", ListaPermisos.Select(p => p.ToString()));
+        return $"Persona: {Id}, {DNI}, {Nombre}, {Apellido}, {Email}, {Telefono}, Permisos: [{permisos}]";
     }
 }
